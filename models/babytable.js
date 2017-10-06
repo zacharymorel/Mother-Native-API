1cfd288854cbd0a6d9cf27dd60e2108549465d76
Zachary Morel <morelz133@gmail.com>
2017-10-06 15:08:25 -0400

    wokring on Babytable associations

--- models/babytable.js
+++ models/babytable.js
@@ -1,16 +1,16 @@
 'use strict';
+
+const User = require('./authouserid')
+
 module.exports = function(sequelize, DataTypes) {
   var BabyTable = sequelize.define('BabyTable', {
     name: DataTypes.STRING,
     duedate: DataTypes.DATE,
     birthdate: DataTypes.DATE,
     gender: DataTypes.STRING,
-    image: DataTypes.STRING,
-    userId: DataTypes.STRING
+    image: DataTypes.STRING
   });
 
-  BabyTable.associate = (models) => {
-    BabyTable.belongsTo(models.AuthOUserId)
-  }
+  BabyTable.belongsTo(User)
   return BabyTable;
 };
